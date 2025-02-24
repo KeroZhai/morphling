e05ad6ebcd6dfef814ffc3a91d1cab32c6184eda
Kero <980580084@qq.com>
2025-02-24 18:41:13 +0800

    docs: add JavaDoc for `Mapper` interface

--- src/main/java/io/github/kerozhai/morphling/mapper/Mapper.java
+++ src/main/java/io/github/kerozhai/morphling/mapper/Mapper.java
@@ -2,18 +2,68 @@ package io.github.kerozhai.morphling.mapper;
 
 import io.github.kerozhai.morphling.annotation.Mapping.ValueStrategy;
 
+/**
+ * Used to map the source object to the target object.
+ */
 public interface Mapper<Source, Target> {
 
-    Target map(Source source, ValueStrategy valueStrategy, Class<?>... ignoreGroups);
+    /**
+     * Generate a new target object by mapping the source object according to the
+     * value strategy and groups.
+     *
+     * @param source        The source object to be mapped.
+     * @param valueStrategy The value strategy to be used.
+     * @param groups        The groups to be used.
+     * @see ValueStrategy
+     * @return The new target object.
+     */
+    Target map(Source source, ValueStrategy valueStrategy, Class<?>... groups);
 
-    void map(Source source, Target target, ValueStrategy valueStrategy, Class<?>... ignoreGroups);
+    /**
+     * Map the source object to the target object according to the value strategy
+     * and groups.
+     *
+     * @param source        The source object to be mapped.
+     * @param target        The target object to be mapped to.
+     * @param valueStrategy The value strategy to be used.
+     * @param groups        The groups to be used.
+     *  @see ValueStrategy
+     */
+    void map(Source source, Target target, ValueStrategy valueStrategy, Class<?>... groups);
 
-    Target map(Source source, Class<?>... ignoreGroups);
+    /**
+     * Generate a new target object by mapping the source object according to the
+     * groups.
+     *
+     * @param source The source object to be mapped.
+     * @param groups The groups to be used.
+     * @return
+     */
+    Target map(Source source, Class<?>... groups);
 
-    void map(Source source, Target target, Class<?>... ignoreGroups);
+    /**
+     * Map the source object to the target object according to the groups.
+     *
+     * @param source The source object to be mapped.
+     * @param target The target object to be mapped to.
+     * @param groups The groups to be used.
+     */
+    void map(Source source, Target target, Class<?>... groups);
 
+    /**
+     * Generate a new target object by mapping the source object.
+     *
+     * @param source The source object to be mapped.
+     * @return The new target object.
+     */
     Target map(Source source);
 
+    /**
+     * Map the source object to the target object.
+     *
+     * @param source The source object to be mapped.
+     * @param target The target object to be mapped to.
+     */
     void map(Source source, Target target);
 
 }
