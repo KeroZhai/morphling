2dbb4bc083917da4a0c99467e312f7162d94cf76
Kero <980580084@qq.com>
2025-04-09 14:13:54 +0800

    fix: ignore static or transient fields

--- src/main/java/io/github/kerozhai/morphling/util/ReflectionUtils.java
+++ src/main/java/io/github/kerozhai/morphling/util/ReflectionUtils.java
@@ -6,6 +6,7 @@ import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.net.Inet4Address;
@@ -36,6 +37,12 @@ public class ReflectionUtils {
 
         while (declaringClass != Object.class) {
             for (Field field : declaringClass.getDeclaredFields()) {
+                int modifiers = field.getModifiers();
+
+                if (Modifier.isStatic(modifiers) || Modifier.isTransient(modifiers)) {
+                    continue;
+                }
+
                 if (fieldNames.add(field.getName())) {
                     fields.add(field);
                 }
